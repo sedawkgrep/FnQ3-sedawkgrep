722e4e7c3c68c4eea6a89d5502036fac0f1d1e53
themuffinator <djdacosta86@yahoo.co.uk>
2026-04-01 23:07:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FnQuake3
+Fappin' Quake 3. It makes you want to fap. Whilst playing Quake 3.
---
+++ code/sdl/sdl_gamma.c
@@ -0,0 +1,57 @@
+/*
+===========================================================================
+Copyright (C) 1999-2005 Id Software, Inc.
+
+This file is part of Quake III Arena source code.
+
+Quake III Arena source code is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
+
+Quake III Arena source code is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Quake III Arena source code; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+===========================================================================
+*/
+
+#ifndef SDL_FUNCTION_POINTER_IS_VOID_POINTER
+#	define SDL_FUNCTION_POINTER_IS_VOID_POINTER 1
+#endif
+
+#include <SDL3/SDL.h>
+
+#include "../client/client.h"
+#include "sdl_glw.h"
+
+void GLimp_InitGamma( glconfig_t *config )
+{
+	config->deviceSupportsGamma = qfalse;
+}
+
+
+/*
+=================
+GLimp_SetGamma
+=================
+*/
+void GLimp_SetGamma( unsigned char red[256], unsigned char green[256], unsigned char blue[256] )
+{
+	(void)red;
+	(void)green;
+	(void)blue;
+}
+
+
+/*
+** GLW_RestoreGamma
+*/
+void GLW_RestoreGamma( void )
+{
+	// automatically handled by SDL?
+}
